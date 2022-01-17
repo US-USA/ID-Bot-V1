3c69356f2172a09cafbd510e1b1856b5de5cc8de
Bot Telegram <95747163+Bottom-T@users.noreply.github.com>
2022-01-17 20:47:12 +0200

    Create Buttons.py

---
+++ BOTidSBot/Commands/Buttons.py
@@ -0,0 +1,26 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+START_BUTTON = InlineKeyboardMarkup( [[
+       InlineKeyboardButton("📢 𝐁𝐨𝐭 𝐔𝐩𝐝𝐚𝐭𝐞𝐬", url=f"t.me/us7a5"),
+       InlineKeyboardButton("𝐎𝐩𝐞𝐧 𝐒𝐨𝐮𝐫𝐜𝐞 💫", url=f"https://github.com/Bottom-T/ID-Bot-V1")
+       ],[
+       InlineKeyboardButton("⬇️ 𝐌𝐨𝐫𝐞 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 ⬇️", callback_data="help")
+       ]]
+       )
+
+HELP_BUTTON = InlineKeyboardMarkup( [[
+       InlineKeyboardButton("𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐈𝐝", callback_data="id"),
+       InlineKeyboardButton("𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐈𝐧𝐟𝐨", callback_data="info")
+       ],[
+       InlineKeyboardButton("🏠 𝐇𝐨𝐦𝐞", callback_data="start"),
+       InlineKeyboardButton("⬇️ 𝐂𝐥𝐨𝐬𝐞", callback_data="close"),
+       InlineKeyboardButton("🤠 𝐀𝐛𝐨𝐮𝐭", callback_data="about")
+       ]]
+       )
+
+ABOUT_BUTTON = InlineKeyboardMarkup( [[
+       InlineKeyboardButton("🔙 𝐁𝐚𝐜𝐤", callback_data="help"),
+       InlineKeyboardButton("🏠 𝐇𝐨𝐦𝐞", callback_data="start"),
+       InlineKeyboardButton("⬇️ 𝐂𝐥𝐨𝐬𝐞", callback_data="close")
+       ]]
+       )

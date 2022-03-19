7433423afe9e77abc52e4dfe173e328b439f66b2
Bot Telegram <95747163+Bottom-T@users.noreply.github.com>
2022-03-19 14:40:18 +0200

    Update Buttons.py

--- BOTidSBot/Commands/Buttons.py
+++ BOTidSBot/Commands/Buttons.py
@@ -1,12 +1,12 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 START_BUTTON = InlineKeyboardMarkup( [[
-       InlineKeyboardButton("💙 𝐁𝐘",  url=f"tg://openmessage?user_id=1257421053"),
-       InlineKeyboardButton("🇺🇲 𝐁𝐘 ", url=f"tg://openmessage?user_id=2146813672")
+       InlineKeyboardButton("💛 𝙱𝚈",  url=f"tg://openmessage?user_id=1257421053"),
+       InlineKeyboardButton("🇺🇲 𝙱𝚈", url=f"tg://openmessage?user_id=2146813672")
        ],[
-       InlineKeyboardButton("⬇️ 𝐌𝐨𝐫𝐞 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 ⬇️", callback_data="help")
+       InlineKeyboardButton("⬇️ 𝙼𝙾𝚁𝙴 𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝚃𝙸𝙾𝙽 ⬇️", callback_data="help")
        ],[
-       InlineKeyboardButton("🪧 𝐋𝐧𝐯𝐢𝐭𝐞 𝐓𝐡𝐞 𝐁𝐨𝐭 𝐓𝐨 𝐓𝐡𝐞 𝐆𝐫𝐨𝐮𝐩",  url=f"https://telegram.me/BOTidSBot?startgroup=start")
+       InlineKeyboardButton("★ 𝙻𝙽𝚅𝙸𝚃𝙴 𝚃𝙷𝙴 𝙱𝙾𝚃 𝚃𝙾 𝚃𝙷𝙴 𝙶𝚁𝙾𝚄𝙿 ★",  url=f"https://telegram.me/BOTidSBot?startgroup=start")
        ]]
        )
 
@@ -14,15 +14,15 @@ HELP_BUTTON = InlineKeyboardMarkup( [[
        InlineKeyboardButton("𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐈𝐝", callback_data="id"),
        InlineKeyboardButton("𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐈𝐧𝐟𝐨", callback_data="info")
        ],[
-       InlineKeyboardButton("🏠 𝐇𝐨𝐦𝐞", callback_data="start"),
-       InlineKeyboardButton("⬇️ 𝐂𝐥𝐨𝐬𝐞", callback_data="close"),
-       InlineKeyboardButton("🤠 𝐀𝐛𝐨𝐮𝐭", callback_data="about")
+       InlineKeyboardButton("🏠 𝙷𝙾𝙼𝙴", callback_data="start"),
+       InlineKeyboardButton("⬇️ 𝙲𝙻𝙾𝚂𝙴", callback_data="close"),
+       InlineKeyboardButton("🐸 𝙰𝙱𝙾𝚄𝚃", callback_data="about")
        ]]
        )
 
 ABOUT_BUTTON = InlineKeyboardMarkup( [[
-       InlineKeyboardButton("🔙 𝐁𝐚𝐜𝐤", callback_data="help"),
-       InlineKeyboardButton("🏠 𝐇𝐨𝐦𝐞", callback_data="start"),
-       InlineKeyboardButton("⬇️ 𝐂𝐥𝐨𝐬𝐞", callback_data="close")
+       InlineKeyboardButton("🔙 𝙱𝙰𝙲𝙺", callback_data="help"),
+       InlineKeyboardButton("🏠 𝙷𝙾𝙼𝙴", callback_data="start"),
+       InlineKeyboardButton("⬇️ 𝙲𝙻𝙾𝚂𝙴", callback_data="close")
        ]]
        )
